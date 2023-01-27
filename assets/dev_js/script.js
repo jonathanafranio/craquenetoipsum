d0cbfc304e5f44076b9a8ec1d09efa28ade3a1fc
Jonathan Afranio <jonathan.afranio.s@google.com>
2023-01-27 19:58:55 -0300

    mudando a aplicacao'

--- assets/dev_js/script.js
+++ assets/dev_js/script.js
@@ -1,28 +1,19 @@
-import { frases } from './frases.min.js'
-
-const gerarParagrafo = (qtd) => {
-    const frasesAleatorias = [];
-    for (let i = 0; i < qtd; i++) {
-        const count = frases.length
-        const indexRandomico = parseInt( Math.random() * count )
-        frasesAleatorias.push(frases[indexRandomico])
-    }
-    return frasesAleatorias.join('\n\n')
-}
-
 document.addEventListener('DOMContentLoaded', () => {
     const resultDiv = document.getElementById('result')
     const formGerar = document.getElementById('gera-paragrafo')
     const copyBtn = document.getElementById('copy-text')
-    
+            
     formGerar.addEventListener('submit', (e) => {
         e.preventDefault();
         const numeroParagrafo = document.getElementById('numero-paragrafo').value
+        const craqueNetoIpsum = window.mIpsum({
+            pNum: numeroParagrafo,
+            tagBefore: '',
+            tagAfter: '\n\n'
+        });
         resultDiv.innerHTML = '';
-        const retornoFrases = gerarParagrafo(numeroParagrafo)
-        resultDiv.append(retornoFrases)
-    })
-
+        resultDiv.innerHTML = craqueNetoIpsum;
+    });
     copyBtn.addEventListener('click', () => {
         resultDiv.select()
         document.execCommand('copy')
